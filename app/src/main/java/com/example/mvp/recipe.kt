60751285476cde24aab4a92521515f19c3ab0e5f
setiywan12 <yayangsetiyawan0@gmail.com>
2019-11-19 21:03:19 +0700

    MVP

---
+++ app/src/main/java/com/example/mvp/recipe.kt
@@ -0,0 +1,12 @@
+package com.example.mvp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class recipe : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_recipe)
+    }
+}
